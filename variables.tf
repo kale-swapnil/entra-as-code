f7f591d6fb25d1034e9db69775a1ddbd01083d84
kale-swapnil <swapnilnkale94@gmail.com>
2026-06-05 19:45:33 +0530

    initial setup

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "tenant_id" {
+  type = string
+}
+
+variable "client_id" {
+  type = string
+}
+
+variable "client_secret" {
+  type = string
+  sensitive = true
+}
+
+variable "deployment_unique_name" {
+  type = string
+}
