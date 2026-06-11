26aee2f26d98960ca5b015ec52b816df18382b67
kale-swapnil <swapnilnkale94@gmail.com>
2026-06-11 19:27:35 +0530

    creating service principal using terraform in entra id

--- main.tf
+++ main.tf
@@ -1,5 +1,11 @@
 module "OidcDebugger_SSO" {
-  source = "./sso_app"
+  source = "./modules/sso_app"
   business_name = "${var.deployment_env_name}-OidcDebuggerSSO"
   web_uri = ["https://oidcdebugger.com/debug"]
+}
+
+module "ServicePrincipal" {
+  source = "./modules/service_principal"
+  business_name = "${var.deployment_env_name}-ServicePrincipal"
+  graph_permissions = ["9a5d68dd-52b0-4cc2-bd40-abcf44ac3a30"]
 }
---
+++ modules/service_principal/main.tf
@@ -0,0 +1,43 @@
+variable "business_name" {
+  description = "Business Name"
+  type = string
+}
+
+variable "deployment_env_name" {
+  description = "Unique name for the deployment"
+  type = string
+  default = "Test"
+}
+
+variable "graph_permissions" {
+    description = "List of Graph API permissions for Role"
+    type = list(string)
+    default = []
+  
+}
+
+resource "azuread_application" "this" {
+  display_name = "TF.${var.deployment_env_name}.${var.business_name}.ServicePrincipal"
+  sign_in_audience = "AzureADMyOrg"
+
+  api {
+    mapped_claims_enabled = true
+    requested_access_token_version = 2
+  }
+
+  required_resource_access {
+    resource_app_id = "00000003-0000-0000-c000-000000000000"
+    dynamic "resource_access" {
+      for_each = var.graph_permissions
+      content {
+        id = resource_access.value
+        type = "Role"
+      }
+    }
+  }
+}
+
+resource "azuread_service_principal" "this" {
+  client_id = azuread_application.this.client_id
+  app_role_assignment_required = false
+}
